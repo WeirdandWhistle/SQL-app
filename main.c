c133f37c321a4a6d4cfcd552578af93a9cb25977
whynotjava <tinycreeperx@gmail.com>
2026-02-03 11:05:16 -0500

    basic SQL testing

--- main.c
+++ main.c
@@ -8,10 +8,46 @@ int main(){
 	sqlite3 *db;
 	int rc;
 
-	rc = sqlite3_open("testdb.db", &db);
+	char cmd[] = "CREATE TABLE IF NOT EXISTS test_table (id int, name text);";
+
+	rc = sqlite3_open_v2("testdb.db", &db, SQLITE_OPEN_READWRITE, NULL);
 	if(rc){
 		printf("OPENING database failed!\n");
+		return 1;
 	}
+	
+	sqlite3_stmt *stmt;
+
+	sqlite3_prepare_v2(db, cmd, -1, &stmt, NULL);
+
+	rc = 1;
+
+	while(rc != SQLITE_DONE){
+		printf("RUNNING sql step! rc: %d\n", rc);
+		rc = sqlite3_step(stmt);		
+	}
+	printf("SQL exited!\n");
+
+
+	sqlite3_finalize(stmt);
+
+	//sqlite3_reset(stmt);
+	
+	rc = sqlite3_prepare_v2(db, "INSERT INTO test_table (id, name) VALUES (1, 'Larry McPadderson');", -1, &stmt, NULL);
+	
+	printf("PREPARE rc: %d\n",rc);
+
+	rc = 1;
+
+	while(rc != SQLITE_DONE){
+		printf("RUNNING sql step! rc: %d\n", rc);
+		rc = sqlite3_step(stmt);		
+	}
+	printf("SQL exited!\n");
+
+
+	sqlite3_finalize(stmt);
+
 	
 
 	sqlite3_close(db);
