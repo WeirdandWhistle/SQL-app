54e2d68a8aaa7baf5867728175ce262327687c9a
whynotjava <tinycreeperx@gmail.com>
2026-02-03 13:44:11 -0500

    db testing

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main(){
 	sqlite3 *db;
 	int rc;
 
-	char cmd[] = "CREATE TABLE IF NOT EXISTS test_table (id int, name text);";
+	char cmd[] = "CREATE TABLE IF NOT EXISTS test_table (id INTEGER PRIMARY KEY, name TEXT NOT NULL);";
 
 	rc = sqlite3_open_v2("testdb.db", &db, SQLITE_OPEN_READWRITE, NULL);
 	if(rc){
@@ -33,7 +33,7 @@ int main(){
 
 	//sqlite3_reset(stmt);
 	
-	rc = sqlite3_prepare_v2(db, "INSERT INTO test_table (id, name) VALUES (1, 'Larry McPadderson');", -1, &stmt, NULL);
+	rc = sqlite3_prepare_v2(db, "INSERT INTO test_table ( name) VALUES ('Larry McPadderson');", -1, &stmt, NULL);
 	
 	printf("PREPARE rc: %d\n",rc);
 
@@ -48,6 +48,26 @@ int main(){
 
 	sqlite3_finalize(stmt);
 
+	
+	sqlite3_prepare_v2(db, "SELECT * FROM test_table;", -1, &stmt, NULL);
+
+	rc = 0;
+	while(rc != SQLITE_DONE){
+		rc = sqlite3_step(stmt);
+
+		if(rc == SQLITE_DONE){
+			break;
+		}
+
+		int col_count = sqlite3_column_count(stmt);
+
+		int id = sqlite3_column_int(stmt, 0);
+		const unsigned char *name = sqlite3_column_text(stmt, 1);
+
+		printf("ENTRY [ID: %d| NAME: %s]\n",id,name);
+			
+		
+	}
 	
 
 	sqlite3_close(db);
