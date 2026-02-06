8095f80f75cf4a80514ca2800eb506dee161e9b6
whynotjava <tinycreeperx@gmail.com>
2026-02-06 16:27:33 -0500

    stuff and layouts and animations and and and and and... whats this? a commit messgae? whats a commit?

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 console.log("HELLO WORKING WORLD!");
 
+const URLforum = document.getElementById("URLforum");
+
 function createNewLink(){
-	alert("YEA!");
+	console.log("opening URL forum");
+	URLForum.style.display = "flex";
 
 	fetch("/api/m",{
 		method: `POST`,
 		body: JSON.stringify({'add':'1234567', 'auth':'token'})
 	});
+}
+function closeURLForum(){
+	console.log("closing URL forum");
+	URLForum.style.display = "none";
 }
